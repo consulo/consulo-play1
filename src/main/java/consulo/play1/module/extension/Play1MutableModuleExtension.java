034a8c1eb451f8219a663fc91ee81010d774afee
VISTALL <vistall.valeriy@gmail.com>
2020-06-03 19:24:13 +0300

    compilation

--- src/main/java/consulo/play1/module/extension/Play1MutableModuleExtension.java
+++ src/main/java/consulo/play1/module/extension/Play1MutableModuleExtension.java
@@ -16,23 +16,28 @@
 
 package consulo.play1.module.extension;
 
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.util.ui.JBUI;
-import consulo.extension.ui.ModuleExtensionSdkBoxBuilder;
-import consulo.module.extension.MutableModuleExtensionWithSdk;
-import consulo.module.extension.MutableModuleInheritableNamedPointer;
-import consulo.roots.ModuleRootLayer;
-import consulo.ui.annotation.RequiredUIAccess;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.swing.*;
+
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.util.ui.JBUI;
+import consulo.disposer.Disposable;
+import consulo.extension.ui.ModuleExtensionSdkBoxBuilder;
+import consulo.module.extension.MutableModuleExtensionWithSdk;
+import consulo.module.extension.MutableModuleInheritableNamedPointer;
+import consulo.module.extension.swing.SwingMutableModuleExtension;
+import consulo.roots.ModuleRootLayer;
+import consulo.ui.Component;
+import consulo.ui.Label;
+import consulo.ui.annotation.RequiredUIAccess;
+import consulo.ui.layout.VerticalLayout;
 
 /**
  * @author VISTALL
  * @since 15:48/28.05.13
  */
-public class Play1MutableModuleExtension extends Play1ModuleExtension implements MutableModuleExtensionWithSdk<Play1ModuleExtension>
+public class Play1MutableModuleExtension extends Play1ModuleExtension implements MutableModuleExtensionWithSdk<Play1ModuleExtension>, SwingMutableModuleExtension
 {
 	public Play1MutableModuleExtension(@Nonnull String id, @Nonnull ModuleRootLayer module)
 	{
@@ -49,7 +54,15 @@ public class Play1MutableModuleExtension extends Play1ModuleExtension implements
 	@RequiredUIAccess
 	@Nullable
 	@Override
-	public JComponent createConfigurablePanel(@Nullable Runnable runnable)
+	public Component createConfigurationComponent(@Nonnull Disposable disposable, @Nonnull Runnable runnable)
+	{
+		return VerticalLayout.create().add(Label.create("Unsupported platform"));
+	}
+
+	@RequiredUIAccess
+	@Nullable
+	@Override
+	public JComponent createConfigurablePanel(@Nonnull Disposable disposable, @Nonnull Runnable runnable)
 	{
 		return JBUI.Panels.verticalPanel().addComponent(ModuleExtensionSdkBoxBuilder.createAndDefine(this, runnable).build());
 	}
